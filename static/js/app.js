8fcdc2a9130265bf192f945b9acfdf6bbb9ea6e2
KyleDamian <khaddock@live.com>
2019-01-06 19:01:23 -0600

    updated code in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -16,7 +16,7 @@ data.forEach((sighting) => {
 
 // add input to search through sightings list
 // Select the submit button
-var submit = d3.select("#submit");
+var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
 
@@ -24,41 +24,13 @@ submit.on("click", function() {
     d3.event.preventDefault();
 
     // Get the value property of the input element
-    var inputValue = d3select("#list-group-item").node().value;
+    var inputValue = d3.select("value");
 
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
 
-    filteredData.forEach((sighting) => {
-        var row = tbody.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-            var cell = tbody.append("td");
-            cell.text(value);
-        });
-    });
+    console.log(filteredData);
+        
 });
 
-// // function to filter table by input results
-// function myFunction() {
-//     // Declare variables
-//     var input, filter, table, tr, td, i, txtValue;
-//     input = document.getElementById("datetime");
-//     filter = input.value();
-//     table = document.getElementById("ufo-table");
-//     tr = table.getElementsByTagName("tr");
-  
-//     // Loop through all table rows, and hide those who don't match the search query
-//     for (i = 0; i < tr.length; i++) {
-//         td = tr[i].getElementsByTagName("td")[0];
-//         if (td) {
-//             txtValue = td.textContent || td.innerText;
-//             if (txtValue.indexOf(filter) > -1) {
-//                 tr[i].style.display = "";
-//             } 
-//             else {
-//                 tr[i].style.display = "none";
-//             }
-        
-//         }
-//     }
-// }
-// figure out how to add filter to table
+
+// figure out how to add multiple filters to table
